f17d6f37c196721f840366af0e0635dd7eb8f8d4
brindha311 <154029167+brindha311@users.noreply.github.com>
2024-02-21 10:23:35 +0100

    Update ss_data.py

--- scripts/web_scraping/ss_data.py
+++ scripts/web_scraping/ss_data.py
@@ -9,6 +9,28 @@ import os
 from deep_translator import GoogleTranslator
 import logging
 import json
+from selenium.webdriver import Chrome
+import requests
+import time
+import csv
+logging.basicConfig(filename='logfile.log', level=logging.DEBUG)
+
+field = "data-science"
+location = "germany"
+
+
+def get_jobs_page(url,filename):
+    driver = Chrome()
+    driver.get(url)
+    time.sleep(30)  # Sleep for 30 seconds
+   # page_source = driver.page_source
+    html = driver.page_source
+    soup = bs(html, 'html.parser')
+
+    #writing the html page in the page_source.html
+    with open(filename, 'w', encoding='utf-8') as html_file:
+        html_file.write(html)
+    return soup
 
 def convert_relative_time_to_date(num,relative_time):
     current_datetime = datetime.now()
@@ -31,14 +53,10 @@ def translate_german_to_english(text):
     translated = translator.translate(text, source='de', target='en')
     return translated
 
-def compute_job_details(source_file):
-    res = open(source_file, "r", encoding="utf8")
-    soup_meta = bs(res, 'html.parser')
-    job_details = soup_meta.find_all("article", class_="res-1p8f8en")
-   # job_details = soup_meta.find_all("article", class_=lambda c: c and c.startswith("res-1"))
+def compute_job_details(soup):
 
+    job_details = soup.find_all("article", class_="res-1p8f8en")
 
-    #print(job_details)
     titles, companys, locations, day_before_posts, day_before_posts_en, date_posts, ref_links = [], [], [], [], [], [], []
     for job_detail in job_details:
         company = job_detail.find("div", class_="res-1r68twq").text.strip()
@@ -61,7 +79,32 @@ def compute_job_details(source_file):
                           columns=["title", "company", "location", "day_before_posts_de", "day_before_posts_en",
                                    "job_posted", "link"])
 
+    print(len(df))
     return df
+
+
+def file_create_append(filename,df_final,csv_df = True):
+
+    records = df_final.to_dict(orient='records')
+
+    append_write = 'a' if os.path.isfile(filename) else 'w'
+    if csv_df:
+        with open(filename, append_write, newline='', encoding='utf-8') as csv_file:
+            fieldnames = records[0].keys() if records else []
+            writer = csv.DictWriter(csv_file,fieldnames = fieldnames)
+            # Write header if the file is being created
+            if append_write == 'w':
+                writer.writeheader()
+            # Write records
+            for record in records:
+                writer.writerow(record)
+
+    else:
+        # Write list of dictionaries to JSON file
+        with open(filename, append_write, encoding='utf-8') as json_file:
+            for record in records:
+                json.dump(record, json_file, ensure_ascii=False)
+                json_file.write('\n')
 
 
 def handler():
@@ -74,30 +117,27 @@ def handler():
         # Create the path to the file in the 'src_data' folder
         src_path = os.path.join(script_dir, 'htmlfiles')
 
+        # final file path for job information(data)
+        write_file_path = script_dir.replace("\scripts\web_scraping", "\data\scraped_data\ss")
+        logging.info(write_file_path)
+        final_file_path_csv = os.path.join(write_file_path, "ss_datascience_germany_20240221.csv")
+        final_file_path_json = os.path.join(write_file_path, "ss_datascience_germany_20240221.json")
+
         df_final = pd.DataFrame()
         # List all files in the directory
-        files = [f for f in os.listdir(src_path) if f.lower().endswith('.html')]
+        for i in range(1,51):
+            URL = f'https://www.stepstone.de/jobs/{field}/in-{location}?radius=100&page={i}'
+            logging.info(URL)
+            html_file_path = os.path.join(src_path, f"ss_20240221_{i}.html")
+            soup = get_jobs_page(URL, html_file_path)
 
-        # Iterate over each CSV file and read into the DataFrame
-        for file_name in files:
-            src_file_path = os.path.join(src_path, file_name)
-            # Read CSV file into corresponding DataFrame
-            df = compute_job_details(src_file_path)
-            df_final = pd.concat([df_final, df], ignore_index=True)
-        write_file_path = script_dir.replace("\scripts\web_scraping", "\data\scraped_data\ss")
-        print(write_file_path)
-        final_file_path_csv = os.path.join(write_file_path, "ss.csv")
-        final_file_path_json = os.path.join(write_file_path, "ss.json")
-        print(len(df_final))
+            df = compute_job_details(soup)
+            file_create_append(final_file_path_csv, df)
+            file_create_append(final_file_path_json, df, False)
+            print(f"iteration {i}, {len(df)} loaded successfully in the path csv {final_file_path_csv} and json {final_file_path_json}")
+            logging.info(f"iteration {i}, {len(df)} loaded successfully in the path csv {final_file_path_csv} and json {final_file_path_json}")
 
-        df_final.to_csv(final_file_path_csv, index=False, header=True)
-        # Write list of dictionaries to JSON file
-        records = df_final.to_dict(orient='records')
-        with open(final_file_path_json, 'w', encoding='utf-8') as json_file:
-            for record in records:
-                json.dump(record, json_file, ensure_ascii=False)
-                json_file.write('\n')
-        print(final_file_path_json)
+
     except Exception as e:
         logging.error(f"An unexpected error occurred in the main block: {e}")
         raise
